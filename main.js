f3b5a8945e5db8225645ed075c3331f2b5f3f8b4
WooJi-Shin <121946458+WooJi-Shin@users.noreply.github.com>
2023-05-15 20:02:11 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Hamdle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  // console.log(event.target.dataset.link);
+  const scrollTO = document.querySelector(link);
+  scrollTO.scrollIntoView({ behavior: "smooth" });
+});
